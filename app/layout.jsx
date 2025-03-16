7a92d5356c135771f33331a6559e44a66712c6d7
Donald Moore <donaldwaynemoorejr@gmail.com>
2025-03-15 23:12:29 -0500

    fix: prod

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,19 +1,10 @@
-// app/layout.tsx
-import { Inter } from "next/font/google";
-import "/styles/stylesheet.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "My App",
-  description: "A description of my app"
-};
+import "./styles.css";
 
 export default function RootLayout({ children }) {
   return (
     <>
       <head></head>
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </>
   );
 }
