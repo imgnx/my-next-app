fb7c9fb542e2c0fcfea07597e766e093cb6ddda8
Donald Moore <donaldwaynemoorejr@gmail.com>
2025-03-15 22:58:31 -0500

    fix: Html element in _document

---
+++ app/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>{/* You can add custom meta tags, stylesheets, etc. here */}</Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
