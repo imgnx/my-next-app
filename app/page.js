996fa3f6d84278f96d1403597ef1ff5ee8d5a54e
Donald Moore <donaldwaynemoorejr@gmail.com>
2025-03-15 22:56:09 -0500

    fix: no pages error

---
+++ app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div style={{ position: "relative", top: 0 }}>
+      <h1
+        className="display"
+        style={{ marginTop: "0", position: "relative", top: "4rem" }}
+      >
+        Imgnxtion!
+      </h1>
+    </div>
+  );
+}
