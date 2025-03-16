2d5fe97ac6ba3ac76d5c8f580dac8bf98afb2b70
Donald Moore <donaldwaynemoorejr@gmail.com>
2025-03-15 23:00:44 -0500

    fix: Html element in _document

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+// app/layout.tsx
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "My App",
+  description: "A description of my app"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head></head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
